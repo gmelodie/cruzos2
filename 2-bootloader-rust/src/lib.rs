5cce898c48770382808297ea8281e15a8496846e
Gabriel Cruz <gabe@gmelodie.com>
2025-10-31 18:58:59 -0300

    fixed bootloader compilation

--- 2-bootloader-rust/src/lib.rs
+++ 2-bootloader-rust/src/lib.rs
@@ -1,45 +1,45 @@
 #![no_std]
 #![no_main]
 
-// use core::arch::asm;
+use core::arch::asm;
 
 #[panic_handler]
 fn panic(_: &::core::panic::PanicInfo) -> ! {
     loop {}
 }
 
-// // Print a string using BIOS teletype (int 0x10, AH=0x0E)
-// unsafe fn println(line: &str) {
-//     let ptr = line.as_ptr();
-//     let mut i = 0u16;
-//     loop {
-//         let c = *ptr.add(i as usize);
-//         if c == 0 {
-//             break;
-//         }
-//         asm!(
-//             "mov ah, 0x0E",
-//             "mov al, {0}",
-//             "int 0x10",
-//             in(reg_byte) c,
-//         );
-//         i += 1;
-//     }
+// Print a string using BIOS teletype (int 0x10, AH=0x0E)
+unsafe fn println(line: &str) {
+    let ptr = line.as_ptr();
+    let mut i = 0u16;
+    loop {
+        let c = *ptr.add(i as usize);
+        if c == 0 {
+            break;
+        }
+        asm!(
+            "mov ah, 0x0E",
+            "mov al, {0}",
+            "int 0x10",
+            in(reg_byte) c,
+        );
+        i += 1;
+    }
 
-//     // Newline
-//     asm!(
-//         "mov ah, 0x0E",
-//         "mov al, 0x0D",
-//         "int 0x10",
-//         "mov al, 0x0A",
-//         "int 0x10",
-//     );
-// }
+    // Newline
+    asm!(
+        "mov ah, 0x0E",
+        "mov al, 0x0D",
+        "int 0x10",
+        "mov al, 0x0A",
+        "int 0x10",
+    );
+}
 
 #[no_mangle]
 pub extern "C" fn bootloader() -> ! {
-    // unsafe {
-    //     println("Hello from Rust 16-bit bootloader!");
-    // }
+    unsafe {
+        println("Hello from Rust 16-bit bootloader!");
+    }
     loop {}
 }
