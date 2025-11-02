8a76843f82e902a47060ad078197ff0975548124
Gabriel Cruz <gabe@gmelodie.com>
2025-11-02 16:14:07 -0300

    MBR data (unused, but better be safe from killer bioses)

--- 2-bootloader-rust/src/lib.rs
+++ 2-bootloader-rust/src/lib.rs
@@ -3,6 +3,11 @@
 
 use core::arch::asm;
 
+// this will be set by linker
+extern "C" {
+    static _STAGE2_START: u16;
+}
+
 #[panic_handler]
 fn panic(_: &::core::panic::PanicInfo) -> ! {
     loop {}
@@ -35,8 +40,10 @@ unsafe fn println(line: &str) {
 #[no_mangle]
 pub extern "C" fn bootloader() -> ! {
     unsafe {
-        println("Hello from Rust 16-bit bootloader!");
-        println("Hello2 from Rust 16-bit bootloader!");
+        println("");
+        println("CruzOS 2 booting...");
     }
+    let stage2_start: *const u16 = unsafe { &_STAGE2_START };
+    // TODO: load from disk using LBA
     loop {}
 }
